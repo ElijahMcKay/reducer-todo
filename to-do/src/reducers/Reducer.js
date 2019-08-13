f9253bf2d35b0ea03b8ce2e93ab1e4a82b32da87
Elijah McKay <elijahmckay10@gmail.com>
2019-08-13 16:21:25 -0500

    setup todo component, initial state, reducer func, and reducer hook

---
+++ to-do/src/reducers/Reducer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const stateObj = {
+    item: 'Learn about reducers', 
+    completed: false, 
+    id: 3892987589,
+}
+
+export const reducerFunc = (state, action) => {
+    return 
+}
